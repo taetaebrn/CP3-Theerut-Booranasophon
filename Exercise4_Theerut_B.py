c0a0e5b58513189266b1e4a636166dbe72ec5fc4
Theerut_b <vhongtei@gmail.com>
2019-02-25 13:46:09 +0700

    Test Score Variable

---
+++ Exercise4_Theerut_B.py
@@ -0,0 +1,9 @@
+fe = 70
+gb = 63
+ics = 90
+cp = 55
+print("---Your Score---")
+print("Foundation English",":",fe)
+print("General Business",":",gb)
+print("Introduction to Computer System",":",ics)
+print("Computer Programming",":",cp)
